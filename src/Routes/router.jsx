b4002662df455d26f0864484d287efe0483b34a2
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2025-04-14 00:27:43 +0600

    updated

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
         path: "donationRequests",
         element: <DonationRequests></DonationRequests>,
       },
-      {
+      { 
         path: "donation-requests/:id",
         element: (
           <PrivateRoute>
